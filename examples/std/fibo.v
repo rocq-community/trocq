c1c85e94ed7b1174e039794c80bb27bdf38265ba
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2025-05-21 11:08:03 +0200

    finish proof of cacheok

--- examples/std/fibo.v
+++ examples/std/fibo.v
@@ -12,13 +12,6 @@ Fixpoint fibo n :=
   end.
 
 Definition cache := nat -> option nat.
-(*Definition state A := cache -> cache * A.
-Definition ret A (x : A) : state A := fun c => (c, x).
-Definition bind A B (x : state A) (f : A -> state B) :=
-  fun c => let (c', a) := x c in f a c'.
-Definition get : state cache := fun c => (c,c).
-Definition set (n v : nat) : state unit :=
-  fun c => ((fun x => if x == n then Some v else c x), tt).*)
 
 Section with_cache.
 Variable M : stateRunMonad cache idfun.
@@ -36,7 +29,9 @@ Fixpoint fibo_memo n : M nat :=
     (do x <-
       match n with
       | 0 => Ret 0
-      | 0.+1 => Ret 0.+1
+      | (0 as n).+1 =>
+          do _ <- fibo_memo n;
+          Ret 0.+1
       | (m.+1 as n).+1 =>
           do x <- fibo_memo m;
           do y <- fibo_memo n;
@@ -70,7 +65,7 @@ by rewrite runStateTret.
 Qed.
 
 Lemma Rlookup_and_compute k n (m : M nat) :
-  R k (maxn k n.+1) m (fibo n) ->
+  (k <= n -> R k (maxn k n.+1) m (fibo n)) ->
   R k (maxn k n.+1) (lookup_or_compute n m) (fibo n).
 Proof.
 move=> Rmm' c Hc.
@@ -97,7 +92,7 @@ Qed.
 
 Lemma Rbind k1 k2 k3 A B (m : M A) m' :
   R k1 k2 m m' -> forall (f : A -> M B) f',
-      (forall a, R k2 k3 (f a) (f' a)) ->
+      (R k2 k3 (f m') (f' m')) ->
       R k1 k3 (m >>= f) (m' >>= f').
 Proof.
 move=> Rmm' f f' Rff' c Hc.
@@ -105,54 +100,56 @@ rewrite runStateTbind.
 case: (Rmm' _ Hc).
 case: (runStateT m c) => n2 c2 /= -> /[dup] Hc2 ->.
 rewrite bindretf /=.
-case: (Rff' m' _ Hc2).
+case: (Rff' _ Hc2).
 rewrite Hc2.
 by move ->.
 Qed.
 
 Lemma cacheok : forall n k,
-    R k (max k n.+1) (fibo_memo n) (fibo n).
+    R k (maxn k n.+1) (fibo_memo n) (fibo n).
 Proof.
 elim/ltn_ind => -[|n] IH k.
+  rewrite [fibo_memo _]/=.
+  apply: Rlookup_and_compute.
+  rewrite bindretf.
+  have -> : fibo 0 = (Ret tt >>= fun=> Ret 0 : (idfun : monad) nat) by [].
+  rewrite leqn0 => /eqP ->.
+  apply: Rbind.
+    exact: Rupdate.
+  exact: Rret.
 rewrite [fibo_memo _]/=.
-  have -> : fibo 0 = (Ret (fibo_trunc k) >>= fun=> Ret 0 : (idfun : monad) nat).
-    by [].
+case: n IH => [|n] IH.
+  apply: Rlookup_and_compute => k1.
+  rewrite bindA.
+  have -> : fibo 1 = Ret 0 >> (Ret tt >> Ret (fibo 1)) :> (idfun : monad) nat by [].
   apply: Rbind.
-    exact: Rget.
-  move=> c.
-  
+    exact: IH.
+  rewrite (bindretf 1).
   apply: Rbind.
-  rewrite runStateTbind runStateTget bindretf /=.
-  move=>
+    have -> : maxn k 1 = 1 by lia.
+    exact: Rupdate.
+  have -> : maxn k 2 = 2 by lia.
+  exact: Rret.
+apply: Rlookup_and_compute => Hk.
+rewrite bindA.
+have -> : fibo n.+2 = Ret (fibo n) >> (Ret (fibo n.+1) >> (Ret tt >> Ret (fibo n.+2))) :> (idfun : monad) nat by [].
+apply: Rbind.
+  exact: IH.
+rewrite bindA.
+apply: Rbind.
+  exact: IH.
+rewrite (bindretf (_ + _)).
+apply: Rbind.
+  rewrite (_ : maxn _ _ = n.+2); last by lia.
+  exact: Rupdate.
+have -> : maxn k n.+3 = n.+3 by lia.
+exact: Rret.
+Qed.
 
-
-Arguments runStateT : simpl never.
-Opaque runStateT.
-
-Lemma cacheok : forall n, R (fibo_memo n) (fibo n).
-Proof.
-elim/ltn_ind => -[|n] IH.
-  move=> c Hc /=.
-  rewrite runStateTbind runStateTget bindretf /=.
-  move: (Hc 0).
-  case: (c 0) => [a|] [] //.
-    case => ->. by rewrite runStateTret.
-  move=> _.
-  rewrite /update.
-  rewrite bindretf runStateTbind runStateTbind /=.
-  rewrite runStateTget bindA bindretf runStateTput.
-  rewrite bindretf runStateTret /=.
-  by split => // -[|n] //=; right.
-rewrite /=.
-
-
+End with_cache.
 
 Fixpoint fibo' n :=
   match n with
   | 0 => (0, 0.+1)
   | m.+1 => let (a,b) := fibo' m in (b, a+b)
   end.
-
-Eval compute 
-
-Definition state
