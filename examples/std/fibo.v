0ef16a745ff6024369d76641703faf8a243ba503
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2025-05-21 14:07:45 +0200

    strengthen lookup_or_compute

--- examples/std/fibo.v
+++ examples/std/fibo.v
@@ -22,22 +22,22 @@ Definition update n v : M unit :=
   do c <- get; let c' x := if x == n then Some v else c x in put c'.
 
 Definition lookup_or_compute n (m : M nat) :=
-  do c <- get; if c n is Some x then Ret x else m.
+  do c <- get;
+  if c n is Some x then Ret x else
+  do x <- m; do _ <- update n x; Ret x.
 
 Fixpoint fibo_memo n : M nat :=
   lookup_or_compute n
-    (do x <-
-      match n with
-      | 0 => Ret 0
-      | (0 as n).+1 =>
-          do _ <- fibo_memo n;
-          Ret 0.+1
-      | (m.+1 as n).+1 =>
-          do x <- fibo_memo m;
-          do y <- fibo_memo n;
-          Ret (x+y)
-      end;
-     do _ <- update n x; Ret x).
+    match n with
+    | 0 => Ret 0
+    | (0 as n).+1 =>
+        do _ <- fibo_memo n;
+        Ret 0.+1
+    | (m.+1 as n).+1 =>
+        do x <- fibo_memo m;
+        do y <- fibo_memo n;
+        Ret (x+y)
+    end.
 
 Definition fibo_trunc k n := if n < k then Some (fibo n) else None.
 
@@ -62,22 +62,6 @@ Lemma Rret k A (a : A) : R k k (Ret a) a.
 Proof.
 move=> c Hc.
 by rewrite runStateTret.
-Qed.
-
-Lemma Rlookup_or_compute k n (m : M nat) :
-  (k <= n -> R k (maxn k n.+1) m (fibo n)) ->
-  R k (maxn k n.+1) (lookup_or_compute n m) (fibo n).
-Proof.
-move=> Rmm' c Hc.
-rewrite /lookup_or_compute.
-rewrite runStateTbind runStateTget bindretf /= Hc /fibo_trunc.
-case: ltnP => nk.
-  rewrite runStateTret /=.
-  split => //. 
-  apply: boolp.funext => x.
-  by have -> : maxn k n.+1 = k by lia.
-rewrite -/(fibo_trunc k) -Hc.
-exact: Rmm'.
 Qed.
 
 Lemma Rupdate k : R k k.+1 (update k (fibo k)) tt.
@@ -105,47 +89,45 @@ rewrite Hc2.
 by move ->.
 Qed.
 
+Lemma Rlookup_or_compute k n (m : M nat) :
+  (k <= n -> R k n m (fibo n)) ->
+  R k (maxn k n.+1) (lookup_or_compute n m) (fibo n).
+Proof.
+move=> Rmm' c Hc.
+rewrite /lookup_or_compute.
+rewrite runStateTbind runStateTget bindretf /= Hc /fibo_trunc.
+case: ltnP => nk.
+  rewrite runStateTret /=.
+  split => //. 
+  apply: boolp.funext => x.
+  by have -> : maxn k n.+1 = k by lia.
+rewrite -/(fibo_trunc _) -Hc.
+move: c Hc.
+rewrite -/(R _ _ _ _).
+have -> : fibo n = Ret (fibo n) >>= fun x => Ret tt >> Ret x
+        :> (idfun : monad) nat by [].
+have -> : maxn k n.+1 = n.+1 by lia.
+exact/(Rbind (Rmm' nk))/Rbind/Rret/Rupdate.
+Qed.
+
 Lemma cacheok : forall n k,
     R k (maxn k n.+1) (fibo_memo n) (fibo n).
 Proof.
-elim/ltn_ind => -[|n] IH k.
-  rewrite [fibo_memo _]/=.
-  apply: Rlookup_or_compute.
-  rewrite bindretf.
-  have -> : fibo 0 = Ret tt >> Ret 0 :> (idfun : monad) nat by [].
+elim/ltn_ind => -[|n] IH k;
+rewrite [fibo_memo _]/=;
+apply: Rlookup_or_compute.
   rewrite leqn0 => /eqP ->.
-  apply: Rbind.
-    exact: Rupdate.
   exact: Rret.
-rewrite [fibo_memo _]/=.
-case: n IH => [|n] IH.
-  apply: Rlookup_or_compute => k1.
-  rewrite bindA.
-  have -> : fibo 1 = Ret 0 >> (Ret tt >> Ret (fibo 1))
-          :> (idfun : monad) nat by [].
-  apply: Rbind.
-    exact: IH.
-  rewrite (bindretf 1).
-  apply: Rbind.
-    have -> : maxn k 1 = 1 by lia.
-    exact: Rupdate.
-  have -> : maxn k 2 = 2 by lia.
+case: n IH => [|n] IH Hk.
+  have -> : fibo 1 = Ret 0 >> Ret (fibo 1) :> (idfun : monad) nat by [].
+  apply: Rbind; first exact: IH.
+  have -> : maxn k 1 = 1 by lia.
   exact: Rret.
-apply: Rlookup_or_compute => Hk.
-rewrite bindA.
-have -> : fibo n.+2 =
-          Ret (fibo n) >> (Ret (fibo n.+1) >> (Ret tt >> Ret (fibo n.+2)))
+have -> : fibo n.+2 = Ret (fibo n) >> (Ret (fibo n.+1) >> Ret (fibo n.+2))
         :> (idfun : monad) nat by [].
-apply: Rbind.
-  exact: IH.
-rewrite bindA.
-apply: Rbind.
-  exact: IH.
-rewrite (bindretf (_ + _)).
-apply: Rbind.
-  rewrite (_ : maxn _ _ = n.+2); last by lia.
-  exact: Rupdate.
-have -> : maxn k n.+3 = n.+3 by lia.
+apply: Rbind; first exact: IH.
+apply: Rbind; first exact: IH.
+rewrite (_ : maxn _ _ = n.+2); last by lia.
 exact: Rret.
 Qed.
 
